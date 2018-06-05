578e946503e002d47d26362b28d5ff78e322aeea
Abdulmumin Abdulkarim <abdulmumineabdulkarim@gmail.com>
2018-06-05 17:49:18 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+//add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '837492174653-j5ecjq55soltql1ivjk7u3ebsi4kb5rf.apps.googleusercontent.com',
+        clientSecret: 'F_U_5dCm3pxbJtNiNX70MYxj'
+    },
+
+    mongodb: {
+        dbURI: 'mongodb://localhost/demosa'
+    },
+    session:{
+        cookieKey:'demosahavegiftedminds'
+    }
+}
